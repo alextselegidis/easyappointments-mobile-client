bcfe8e8fc5708f2db8dc4ebc657e9ecc949f7601
Matteo Venturi <matteo.venturi7@gmail.com>
2017-04-20 12:52:14 +0200

    added libs activeandroid and retrofit first config activeandroid

---
+++ app/src/main/java/com/easyappointments/MyApp.java
@@ -0,0 +1,10 @@
+package com.easyappointments;
+
+import com.activeandroid.app.Application;
+
+/**
+ * Created by matte on 20/04/2017.
+ */
+
+public class MyApp extends com.activeandroid.app.Application {
+}
